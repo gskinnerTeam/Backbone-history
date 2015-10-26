01075bf1a88c35dad6a91c5d829364c0087aebb4
Patrick McGuckin <patrick@gskinner.com>
2015-10-26 15:17:51 -0600

    Updates in progress.

---
+++ js/core/main.js
@@ -0,0 +1,50 @@
+/**
+ * Application controller.
+ * @class Main
+ */
+(function (scope) {
+
+	'use strict';
+
+	function Main () {
+		scope.router = new scope.Router();
+		scope.router.on('route', this.updateView, this);
+		this.container = $('#container');
+
+		this.parseFirstRoute();
+		Backbone.history.start({ pushState: true });
+	}
+
+	var p = {};
+
+	/**
+	 * Remove the current view and inject a new one.
+	 * @method updateView
+	 */
+	p.updateView = function () {
+		this.activeView && this.activeView.remove();
+		this.activeView = new scope.View();
+		this.container.append(this.activeView.el);
+	};
+
+	/**
+	 * Check the first route for hashbangs.
+	 * @method parseFirstRoute
+	 */
+	p.parseFirstRoute = function () {
+		var route = window.location.hash;
+		if (/^#!\//.test(route)) {
+			route = route.substr(3); // strip hashbang
+			// rewrite the hash, since it is what Backbone reads when start is called.
+			if (!!window.history) {
+				window.history.replaceState({}, '', route);
+			} else {
+				window.location.replace(route);
+			}
+		}
+	};
+
+	 _.extend(Main.prototype, p);
+	scope.Main = Main;
+
+})(window.app);
---
+++ js/routers/router.js
@@ -0,0 +1,57 @@
+/**
+ * Application router.
+ * @class Router
+ * @extends Backbone.Router
+ */
+(function (scope) {
+
+	'use strict';
+
+	var p = {};
+	var s = {};
+
+	/**
+	 * Backbone's routes object.
+	 * @property routes
+	 * @type {object}
+	 */
+	p.routes = {
+		'': 'home',
+		'fruits(/:fruit)': 'fruit',
+		'*error': 'error'
+	};
+
+	/**
+	 * Shorthand for Router.navigate that lets us run processes before the route is sent.
+	 * @method go
+	 * @param dest
+	 */
+	p.go = function (dest) {
+		if (this.activeRoute === dest) { return; }
+		this.activeRoute = dest;
+		this.navigate(dest, { trigger: true });
+	};
+
+	/**
+	 * Compares the live site origin to the origin on the anchor's href to determine whether or not it is an internal route.
+	 * @method detectInternalLink
+	 * @param link
+	 * @returns {boolean}
+	 */
+	p.detectInternalLink = function (link) {
+		// spike-only crawler check
+		if (link.className === 'crawler') { return false; }
+		// link.href === “http://site.com/path/to/view”
+		var destination = link.href;
+		var currentBase = window.location.protocol + '//' + window.location.hostname;
+		// if bases match, check the path against exceptions
+		if (destination.indexOf(currentBase) >= 0) {
+			var exceptions = /(some\/directory|other\/directory)\/.+/i;
+			return destination.split(currentBase)[1].match(exceptions) === null;
+		}
+		return false;
+	};
+
+	scope.Router = Backbone.Router.extend(p, s);
+
+})(window.app);
